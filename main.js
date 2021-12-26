884e4db40262c6e9d7505da39b6d0af6c8be882c
Vilém Haupt <71026000+vilemhaupt@users.noreply.github.com>
2021-12-26 14:08:41 +0100

    Uprava souboru
    Upravená funkčnost / Vylepšený kod
    
    - Opravená chyba při špatném čísle

--- main.js
+++ main.js
@@ -2,46 +2,52 @@
 const speedInput = document.getElementById("speed");
 const r1h = document.getElementById("speed1h");
 const err = document.getElementById("err");
-const speedErr = document.getElementById("speedr");
-const errWarn = document.getElementById("err-warn");
 
-const maxSupportedValue = 100000000000000000000; // Proměnná která zajištuje aby se program nepokazil
 
-var calculate = () => { // Funkce díky které celý program funguje jak má
 
-    const speedValue = speedInput.value; // Zjištění čísla z inputu 
+// Proměnná která zajištuje aby se program nepokazil
 
-    const kmpH = speedValue * 3.6; // Proměnná která vypočítá průměrnou rychlost v hodině 
+const maxSupportedValue = 100000000000000000000; 
+
+// Funkce díky které celý program funguje jak má
+
+var calculate = () => { 
+
+    // Zjištění čísla z inputu 
+
+    const speedValue = speedInput.value; 
+
+    // Proměnná která vypočítá průměrnou rychlost v hodině 
+
+    const kmpH = speedValue * 3.6; 
 
     r1h.innerHTML = kmpH + " km/h ";
     err.innerHTML = "";
 
-    if (speedValue == 0){ // Zabrání chodu systému pokud uživatel zadal hodnotu 0, nebo ji nevyplnil
+    // Zabrání chodu systému pokud uživatel zadal hodnotu 0, nebo ji nevyplnil
+
+    if (speedValue == 0){ 
 
         r1h.innerHTML = "";
         err.innerHTML =  "Rychlost nemůže být 0 / nezadaná!";
 
-        speedErr.classList.add("hidden");
-        errWarn.classList.remove("hidden");
-    }
 
-
-
-    if (speedValue == maxSupportedValue || speedValue > maxSupportedValue ){ // Zabrání chodu systému pokud uživatel použil větší hodnotu než je povolená
+    } else if (speedValue >= maxSupportedValue){
         r1h.innerHTML = "";
-        err.innerHTML =  "Rychlost v m/s nemůže být větší než" + " " + maxSupportedValue;
-        
-        speedErr.classList.add("hidden");
-        errWarn.classList.remove("hidden");
-    }
+        err.innerHTML =  "Rychlost v m/s nemůže být větší než " + maxSupportedValue;
+ 
 
-    if (speedValue < 0){ // Zabrání chodu systému pokud číslo je menší než 0 (-1 atd)   
+    } else if (speedValue < 0) { 
         r1h.innerHTML = "";
         err.innerHTML =  "Rychlost v m/s nemůže být menší než 0" 
         
-        speedErr.classList.add("hidden");
-        errWarn.classList.remove("hidden");
+
     }
+
+
+
+
+
 
 }
 
